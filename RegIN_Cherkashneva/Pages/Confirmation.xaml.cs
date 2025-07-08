e22da0c230de4ec7f1c8c90b6111a6b8e1578bcb
pepptyxa <pheronerxe@gmail.com>
2025-07-08 19:46:15 +0500

    Программирование окна Confirmation. Черкашнева

--- RegIN_Cherkashneva/Pages/Confirmation.xaml.cs
+++ RegIN_Cherkashneva/Pages/Confirmation.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,14 +16,78 @@ using System.Windows.Shapes;
 
 namespace RegIN_Cherkashneva.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для Confirmation.xaml
-    /// </summary>
     public partial class Confirmation : Page
     {
+        public enum TypeConfirmation
+        {
+            Login,
+            Regin
+        }
+        TypeConfirmation ThisTypeConfirmation;
+        public int Code = 0;
         public Confirmation()
         {
             InitializeComponent();
+        }
+        public void SendMailCode()
+        {
+            Code = new Random().Next(100000, 999999);
+            Classes.SendMail.SendMessage($"Login code: {Code}", MainWindow.mainWindow.UserLogIn.Login);
+            Thread TSendMailCode = new Thread(TimerSendMailCode);
+            TSendMailCode.Start();
+        }
+        public Confirmation(TypeConfirmation TypeConfirmation)
+        {
+            InitializeComponent();
+            ThisTypeConfirmation = TypeConfirmation;
+            SendMailCode();
+        }
+        public void TimerSendMailCode()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    LTimer.Content = $"A second message can be sent after {(60 - i)} seconds";
+                });
+                Thread.Sleep(1000);
+            }
+            Dispatcher.Invoke(() =>
+            {
+                BSendMessage.IsEnabled = true;
+                LTimer.Content = "";
+            });
+        }
+        private void SendMail(object sender, RoutedEventArgs e)
+        {
+            SendMailCode();
+        }
+        private void SetCode(object sender, KeyEventArgs e)
+        {
+            if (TbCode.Text.Length == 6)
+                SetCode();
+        }
+        private void SetCode(object sender, RoutedEventArgs e) =>
+            SetCode();
+        void SetCode()
+        {
+            if (TbCode.Text == Code.ToString() && TbCode.IsEnabled)
+            {
+                TbCode.IsEnabled = false;
+                if (ThisTypeConfirmation == TypeConfirmation.Login)
+                {
+                    MessageBox.Show("Авторизация пользователя успешно подтверждена");
+                }
+                else
+                {
+                    MainWindow.mainWindow.UserLogIn.SetUser();
+                    MessageBox.Show("Регистрация пользователя успешно подтверждена");
+                }
+            }
+        }
+        private void OpenLogin(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Login());
         }
     }
 }
